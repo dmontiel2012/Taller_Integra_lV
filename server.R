8a6af07dd7e4b2f294740890e02d066fe549212c
jcvergara06 <juan.vergara.06@gmail.com>
2016-11-15 05:05:07 -0300

    Add files via upload

--- server.R
+++ server.R
@@ -1,20 +1,96 @@
+
+############################################################################################################
+############################################################################################################
+
 library(shiny)
 library(leaflet)
 library(xlsx)
 library(htmltools)
 
-datos <- data.frame(read.xlsx("C:/Users/JCFunk/Downloads/Colegios Temuco.xlsx", sheetName = "Hoja1"))
-datos
+#datos <- data.frame(read.xlsx("C:/Users/JCFunk/Downloads/Colegios Temuco.xlsx", sheetName = "Hoja1"))
+#datos
+
+datos <- data.frame(read.csv("C:/Users/JCFunk/Downloads/escuelasFinal.csv"))
+
 
 ## renderLeaflet() se utiliza del lado del servidor para hacer el mapa
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output,session) {
   output$mymap <- renderLeaflet({
     # definir el objeto de mapa
     
-    leaflet(datos) %>% addTiles() %>%
+    leaflet(datos) %>% addTiles() %>% 
+      setView(lng = -72.5845, lat = -38.7338, zoom = 12) %>%
+      #addCircleMarkers(lng = -(runif(500,72.5,72.8)), -(runif(500,38.6,38.76)), popup = ~htmlEscape("Dario Montiel"),color="RED")%>%
       addMarkers(~Longitud, ~Latitud, popup = ~htmlEscape(Nombres))
+
+  })
+  
+  observeEvent(input$button, {
+    
+    output$mymap <- renderLeaflet({
+      # definir el objeto de mapa
+      
+      leaflet(datos) %>% addTiles() %>% 
+        setView(lng = -72.5845, lat = -38.7338, zoom = 12) %>%
+        addCircleMarkers(lng = -(runif(500,72.5,72.8)), -(runif(500,38.6,38.76)), popup = ~htmlEscape("Dario Montiel"),color="RED")%>%
+        addMarkers(~Longitud, ~Latitud, popup = ~htmlEscape(Nombres))
+      
+    })
+    
+  })
+  
+  output$plot <- renderPlot({
+    input$newplot
+    
+    ggplot()+
+      geom_line(aes(x=t,y=vmincostos))+
+      geom_line(aes(x=t,y=vmaxcostos),col="red")+
+      geom_line(aes(x=t,y=vmediancostos),col="blue")+
+      geom_hline(yintercept=calCosto(masCerca,alpha),col="black",lty=2)
+    
+  })
+  
+  output$plot2 <- renderPlot({
+    input$newplot
+    
+    ggplot()+
+      geom_line(aes(x=t,y=minS),col="darkgreen")+
+      geom_hline(yintercept=S(masCerca),col="black",lty=2)
+    
+  })
+  
+  output$plot3 <- renderPlot({
+    input$newplot
+    
+    ggplot()+
+      geom_line(aes(x=t,y=vmeanCostCupo),col="darkgreen")+
+      geom_hline(yintercept=costCupo(masCerca),col="black",lty=2)
+    
+  })
+  
+  output$plot4 <- renderPlot({
+    input$newplot
+    
+    ggplot()+
+      geom_line(aes(x=t,y=vmeanDist),col="darkgreen")+
+      geom_hline(yintercept=meanDist(masCerca),col="black",lty=2)
+    
+  })
+  
+  output$plot5 <- renderPlot({
+    input$newplot
+   
+  
+
+  })
+  
+  output$prueba <- DT::renderDataTable({
+    
+    datos <- read.csv("C:/Users/JCFunk/Downloads/escuelasFinal.csv")
+    datos
     
   })
   
 })
+
--- ui.R
+++ ui.R
@@ -2,7 +2,61 @@ library(shiny)
 library(leaflet)
 ## leafletOutput se utiliza en el lado de la interfaz de usuario para mostrar el mapa .
 
+
+
 shinyUI(fluidPage(
-  leafletOutput("mymap")
   
+  navbarPage("Taller IV", id="nav",
+             
+             tabPanel("Mapa",
+                      div(class="outer"),
+                      
+                      tags$head(
+                        # Include our custom CSS
+                        includeCSS("styles.css"),
+                        includeScript("gomap.js")
+                      ),
+                      
+                      absolutePanel(id = "controls2", class = "panel panel-default",fixed = FALSE,
+                                    draggable = FALSE, top = 60, left = NULL, right = "auto", bottom = "auto",
+                                    width = 600, height = 560,
+                                    
+                                    leafletOutput("mymap",width = 600, height = 560),
+                                    actionButton("button", "General Estudiantes")
+                                    
+                                    
+                                    ),
+                      
+                      
+                      #leafletOutput("mymap",width = 500, height = 500),
+                      
+                      absolutePanel(id = "controls", class = "panel panel-default",fixed = FALSE,
+                                    draggable = FALSE, top = 60, left = "780", right = NULL, bottom = "auto",
+                                    width = 600, height = 680,
+                                    h2("Graficos"),
+                                    wellPanel(id = "tPanel",style = "overflow-y:scroll; max-height: 600px",
+                                    
+                                    
+                                    plotOutput("plot", height=300),
+                                    plotOutput("plot2", height=300),
+                                    plotOutput("plot3", height=300),
+                                    plotOutput("plot4", height=300),
+                                    plotOutput("plot5", height=300)
+                                    )),
+                      tags$div(id="cite",
+                               'Prueba Texto ', tags$em('Proyecto: Taller de integracion 4'), ' Por Juan Carlos Vergara (Taller 4, 2015).'
+                      )
+                      
+             ),
+             tabPanel("Datos",
+             
+             hr(),
+             DT::dataTableOutput("prueba")
+  ),
+  
+  conditionalPanel("false", icon("crosshair"))
+  
+  )
 ))
+
+
